feb6c0fa8b5be9778bb9608e46fcddb2669215ec
aftero69 <learnsyed9@gmail.com>
2021-04-24 02:04:09 +0530

    Add salting to hashing(bcrypt).

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const app = express();
@@ -40,26 +41,34 @@ app.get("/register", function (req, res) {
 })
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save()
-        .then(() => res.render("secrets"))
-        .catch((err) => console.log(err));
+    // this takes the password from user and salts .Meaning it hashes the password times  to salRound.
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save()
+            .then(() => res.render("secrets"))
+            .catch((err) => console.log(err));
+    });
 });
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({
         email: username
     }).then((foundUser) => {
         if (foundUser) {
-            if (foundUser.password === password) {
-                res.render("secrets")
-            }
+            bcrypt.compare(password, foundUser.password, function (err, result) {
+                if (result) {
+                    res.render("secrets");
+                } else {
+                    console.log(err);
+                }
+
+            });
         }
     }).catch((err) => {
         console.log(err);
